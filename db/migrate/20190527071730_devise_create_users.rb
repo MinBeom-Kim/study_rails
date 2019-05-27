3dd876af29bdd50153e5e9976afb39f528c25c4a
mbk <kmb3755@gmail.com>
2019-05-27 19:07:06 +0900

    divise_user

--- db/migrate/20190527071730_devise_create_users.rb
+++ db/migrate/20190527071730_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :user_name, null: false, default: ""
+      t.string :user_phone, null: false, default: ""
+      t.string :profile
 
       ## Recoverable
       t.string   :reset_password_token
