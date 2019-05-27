d0dda6c0f8d759427555cb39ca60ff154e90350c
mbk <kmb3755@gmail.com>
2019-05-27 20:35:43 +0900

    user_validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 유효성 검사 : 사용자 이름과 휴대전화가 입력되지 않으면 에러 메시지 출력
+  validates :user_name, :user_phone, :presence => true
 end
