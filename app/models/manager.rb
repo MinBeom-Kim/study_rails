7c85cdd1ba03dfef1b13c78d31cc4a88ffcb5738
mbk <kmb3755@gmail.com>
2019-05-27 21:21:20 +0900

    has_many

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,4 @@
 class Manager < ApplicationRecord
+    has_many :studies
+    has_many :study_has_managers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # 유효성 검사 : 사용자 이름과 휴대전화가 입력되지 않으면 에러 메시지 출력
   validates :user_name, :user_phone, :presence => true
+
+  has_many :stydues
+  has_many :user_has_studies
 end
