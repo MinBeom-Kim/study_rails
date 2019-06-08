cf7de51ccd98350e1e1725273eaea1e5da3c7201
cosmos <kmb3755@gmail.com>
2019-06-08 20:13:04 +0900

    sort

--- app/models/study.rb
+++ app/models/study.rb
@@ -6,6 +6,9 @@ class Study < ApplicationRecord
   # 유효성 검사 : 모든 입력 폼이 입력되지 않으면 에러 메시지 출력
   validates :study_name, :intro, :goal, :curriculem, :max_number, :presence => true
 
+  # default 정렬 : 스터디 이름을 가나다 순으로 정렬
+    default_scope { order(study_name: :ASC)}
+
   # 스터디 개설 유무 확인
   def study_status?
     if self.status == true
