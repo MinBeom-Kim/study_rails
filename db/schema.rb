89da231102deef507c5cad97b27eb610898e6623
MinBeom-Kim <kmb3755@gmail.com>
2020-05-07 03:22:55 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord:
+ActiveRecord::Schema.define(version: 2019_06_09_152024) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
